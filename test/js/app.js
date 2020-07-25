39a36d55e0687b5f8b4b8887fb8544cbce1b750d
Rohit Negi <rohit5822540@gmail.com>
2020-07-25 21:25:49 +0530

    update test

--- test/js/app.js
+++ test/js/app.js
@@ -1,4 +1,6 @@
 var Controller=(function(){
+    var waitTime=false,scroll_limit,y,middle,keep;
+    var breaks=[];
     var selector={
         box1:document.querySelector('.box1'),
         box2:document.querySelector('.box2'),
@@ -43,42 +45,157 @@ var Controller=(function(){
     };
     var SetupEventList=function(){
         window.addEventListener("scroll",function(e){
-            var y=window.scrollY;
+            y=window.scrollY;
             if(y>200){
-                selector.box1.style.left='50%';
-                selector.box2.style.right='50%';    
+                //selector.box1.style.left='50%';
+                //selector.box2.style.right='50%';    
                 selector.shade.style.display="block";
                 selector.mode.style.display="none"
             }
             else{
-                selector.box1.style.left='-300px';
-                selector.box2.style.right='-300px';
+                //selector.box1.style.left='-300px';
+                //selector.box2.style.right='-300px';
                 selector.shade.style.display="none";
                 selector.mode.style.display="block";
             }
-            if(y>400){
-                selector.box3.style.left="50%";
-                selector.box4.style.right="50%";
-            }
-            else{
-                selector.box3.style.left="-300px";
-                selector.box4.style.right="-300px";
-            }
-            if(y>600){
-                selector.box5.style.left="50%";
-                selector.box6.style.right="50%";
-            }
-            else{
-                selector.box5.style.left="-300px";
-                selector.box6.style.right="-300px";
-            }
+            // if(y>400){
+            //     selector.box3.style.left="50%";
+            //     selector.box4.style.right="50%";
+            // }
+            // else{
+            //     selector.box3.style.left="-300px";
+            //     selector.box4.style.right="-300px";
+            // }
+            // if(y>600){
+            //     selector.box5.style.left="50%";
+            //     selector.box6.style.right="50%";
+            // }
+            // else{
+            //     selector.box5.style.left="-300px";
+            //     selector.box6.style.right="-300px";
+            // }
         });
         selector.mode.addEventListener("click",function(e){
             ChangeColor();
         });
     };  
+    var WaitControl=setInterval(()=>{
+        waitTime=true;
+    },50);
+    var HideAllLayer=()=>{
+        selector.box1.style.left='-50%';
+        selector.box2.style.right='-50%';
+        selector.box3.style.left='-50%';
+        selector.box4.style.right='-50%';
+        selector.box5.style.left='-50%';
+        selector.box6.style.right='-50%';
+    };
+    var ShowAllLayer=()=>{
+        selector.box1.style.left='50%';
+        selector.box2.style.right='50%';
+        selector.box3.style.left='50%';
+        selector.box4.style.right='50%';
+        selector.box5.style.left='50%';
+        selector.box6.style.right='50%';
+    };
+    var CheckLayerOne=(left,y,right)=>{
+        left=parseInt(left);
+        right=parseInt(right);
+        y=parseInt(y);
+        middle=parseInt(((-left+right)/2+left).toFixed(2));
+        //console.log(left,middle,right,y);
+        if(y<middle){
+            keep=parseInt((((y-left)/(middle-left))*50).toFixed(2))-50;
+            selector.box1.style.left=`${keep}%`;
+            selector.box2.style.right=`${keep}%`;
+            selector.box3.style.left="-50%";
+            selector.box4.style.right="-50%";
+            
+            //console.log(keep);
+        }
+        else{
+            keep=(((y-middle)/(right-middle))*50).toFixed(2);
+            selector.box1.style.left=`${keep}%`;
+            selector.box2.style.right=`${keep}%`;
+            //console.log(keep);
+        }
+    };
+    var CheckLayerTwo=(left,y,right)=>{
+        left=parseInt(left);
+        right=parseInt(right);
+        y=parseInt(y);
+        middle=parseInt(((-left+right)/2+left).toFixed(2));
+        selector.box1.style.left="50%";
+        selector.box2.style.right="50%";
+        selector.box5.style.left="-50%";
+        selector.box6.style.right="-50%";
+        //console.log(left,middle,right,y);
+        if(y<middle){
+            keep=parseInt((((y-left)/(middle-left))*50).toFixed(2))-50;
+            selector.box3.style.left=`${keep}%`;
+            selector.box4.style.right=`${keep}%`;
+            //console.log(keep);
+        }
+        else{
+            keep=(((y-middle)/(right-middle))*50).toFixed(2);
+            selector.box3.style.left=`${keep}%`;
+            selector.box4.style.right=`${keep}%`;
+            //console.log(keep);
+        }
+    };
+    var CheckLayerThree=(left,y,right)=>{
+        left=parseInt(left);
+        right=parseInt(right);
+        y=parseInt(y);
+        middle=parseInt(((-left+right)/2+left).toFixed(2));
+        selector.box3.style.left="50%";
+        selector.box4.style.right="50%";
+        //console.log(left,middle,right,y);
+        if(y<middle){
+            keep=parseInt((((y-left)/(middle-left))*50).toFixed(2))-50;
+            selector.box5.style.left=`${keep}%`;
+            selector.box6.style.right=`${keep}%`;
+            //console.log(keep);
+        }
+        else{
+            keep=(((y-middle)/(right-middle))*50).toFixed(2);
+            selector.box5.style.left=`${keep}%`;
+            selector.box6.style.right=`${keep}%`;
+            //console.log(keep);
+        }
+    };
+    var FullControl=function(){
+        window.addEventListener("scroll",function(e){
+            if(waitTime){
+                waitTime=false;
+                y=window.scrollY;
+                scroll_limit=parseInt(document.body.scrollHeight-screen.height);
+                //console.log(y,scroll_limit);
+                breaks[0]=(scroll_limit*0.125).toFixed(2);
+                breaks[1]=(scroll_limit*0.375).toFixed(2);
+                breaks[2]=(scroll_limit*0.625).toFixed(2);
+                breaks[3]=(scroll_limit*0.875).toFixed(2);
+                if(y<breaks[0]){
+                    HideAllLayer();
+                }
+                else if(y>=breaks[0] && y<breaks[1]){
+                    CheckLayerOne(breaks[0],y,breaks[1]);
+                }
+                else if(y>=breaks[1] && y<breaks[2]){
+                    CheckLayerTwo(breaks[1],y,breaks[2]);
+                }
+                else if(y>=breaks[2] && y<breaks[3]){
+                    CheckLayerThree(breaks[2],y,breaks[3]);
+                }
+                else{
+                    ShowAllLayer();
+                }
+            }
+        });
+    };
     return{
         init:function(){
+            FullControl();
             SetupEventList();
             ChangeColor();
         }
